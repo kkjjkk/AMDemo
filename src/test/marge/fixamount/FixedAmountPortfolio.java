fa2c82a24ae3172dc7487c0c760126cd18baf32c
plum <275039553@qq.com>
2019-06-20 11:29:19 +0800

    数学和算法测试项目

---
+++ src/test/marge/fixamount/FixedAmountPortfolio.java
@@ -0,0 +1,21 @@
+package test.marge.fixamount;
+
+/**
+ * 固定金额组合测试
+ * <p>
+ * 有[1, 2, 5, 10]四种面额的钱，用组合的方式凑成10元整，有多少种组合方式（去重）
+ *
+ * @author plum
+ * 2018-06-20
+ */
+public class FixedAmountPortfolio {
+
+    /**
+     * 测试方法
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
