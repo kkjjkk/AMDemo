da1182f328aa1b39fbf814a1192d5b19fbb93884
plum <275039553@qq.com>
2019-11-13 18:56:24 +0800

    基数排序

---
+++ src/main/java/com/demo/sort/RadixSort.java
@@ -0,0 +1,78 @@
+package com.demo.sort;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 基数排序，在计数排序的基础上，进行逐层的拆分排序，即进行多次的计数排序，最终打到排序的目的
+ * 1.有一个硬性要求，就是作为基础的计数排序阶段，必须是稳定排序，所以本次实现的计数排序部分会比基础版的计数排序更复杂
+ * 2.用字符串排序为例子，演示基数排序的逐层推进操作
+ */
+public class RadixSort {
+
+    private static final Logger logger = LoggerFactory.getLogger(RadixSort.class);
+
+    /**
+     * 基数排序
+     *
+     * @param list
+     * @return
+     */
+    private static String[] radixSort(String[] list) {
+        // 找出最长的字符数
+        int maxSize = 0;
+        for (String s : list) {
+            if (list.length > maxSize) {
+                maxSize = list.length;
+            }
+        }
+
+        // 临时数组，用于对每一位上的字母ASCII编码进行排序
+        String[] temp = new String[list.length];// 临时拷贝数组
+        // 需要做maxSize轮排序
+        for (int i = 0; i < maxSize; i++) {
+            int[] charList = new int[128];// ASCII的取值范围
+            for (int j = 0; j < list.length; j++) {
+                String s = list[j];
+                // 如果位不够，就不变动已有顺序了
+                if (s.length() > i) {
+                    charList[s.charAt(i)]++;
+                }
+            }
+
+            // 进行稳定版本的计数排序
+            // 1.将charList的元素值变形，当前元素的值是前一个元素的值+当前值（代表了在当前位置之前，还有多少个元素）
+            for (int j = 1; j < charList.length; j++) {
+                charList[j] += charList[j - 1];
+            }
+
+            logger.info("第{}轮：charList={}", i, charList);
+
+            // 2.倒叙遍历list，将匹配的元素填到temp中
+            for (int j = list.length - 1; j >= 0; j--) {
+                if (list[j].length() > i) {
+                    int tempIndex = charList[list[j].charAt(i)] - 1;// 计算出temp的坐标
+                    temp[tempIndex] = list[j];
+                    charList[list[j].charAt(i)]--;
+                }
+            }
+
+            logger.info("第{}轮：temp={}", i, temp);
+
+            // 3.将第i轮排序的temp拷贝到list中，进行下一轮排序
+            list = temp.clone();
+        }
+
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        String[] list = new String[]{"abc", "aa", "bc", "ddef", "bbc", "hk", "china", "mmp", "mdzz", "xml"};
+        logger.info("oldList : {}", JSONObject.toJSONString(list));
+
+        String[] result = radixSort(list);
+        logger.info("newList : {}", JSONObject.toJSONString(result));
+    }
+}
